bf5d5bc77eef74308b4729c25b7b0ab77951840d
Francesco Piero Paolicelli <piersoft@me.com>
2016-06-25 23:45:53 +0200

    bugfix

--- main.php
+++ main.php
@@ -1,6 +1,6 @@
 <?php
 /**
-* Telegram Bot LibreItalia
+* Telegram Bot Napoli Fly
 * @author Francesco Piero Paolicelli @piersoft
 */
 
@@ -30,7 +30,11 @@ function start($telegram,$update)
 {
 	date_default_timezone_set('Europe/Rome');
 	$today = date("Y-m-d H:i:s");
-
+if (strpos($text,'❕') === false || strpos($text,'🛫') === false || strpos($text,'🛬') === false){
+	$text=str_replace("❕ ","",$text);
+	$text=str_replace("🛫 ","",$text);
+	$text=str_replace("🛬 ","",$text);
+}
 	if ($text == "/start" || $text == "Informazioni") {
 		$img = curl_file_create('logo.png','image/png');
 		$contentp = array('chat_id' => $chat_id, 'photo' => $img);
@@ -52,53 +56,6 @@ function start($telegram,$update)
 		}
 
 		elseif($text == "Arrivi")
-		{
-
-
-				$urlgd  ="https://spreadsheets.google.com/tq?tqx=out:csv&tq=SELECT%20%2A%20&key=".GDRIVEKEY."&gid=".GDRIVEGID2;
-				sleep (1);
-				$inizio=1;
-				$homepage ="";
-				//$comune="Lecce";
-
-				//echo $urlgd;
-				$csv = array_map('str_getcsv',file($urlgd));
-				//var_dump($csv[1][0]);
-				$count = 0;
-				foreach($csv as $data=>$csv1){
-					$count = $count+1;
-				}
-
-					function decode_entities($text) {
-
-													$text=htmlentities($text, ENT_COMPAT,'ISO-8859-1', true);
-												$text= preg_replace('/&#(\d+);/me',"chr(\\1)",$text); #decimal notation
-													$text= preg_replace('/&#x([a-f0-9]+);/mei',"chr(0x\\1)",$text);  #hex notation
-												$text= html_entity_decode($text,ENT_COMPAT,"UTF-8"); #NOTE: UTF-8 does not work!
-				return $text;
-					}
-				for ($i=$inizio;$i<$count;$i++){
-
-
-					$homepage .="\n";
-					$homepage .="Volo N°: ".$csv[$i][0]."\n";
-					$homepage .="Da: ".$csv[$i][1];
-					if ($csv[$i][2] != null) $homepage .=" Aereoporto: ".$csv[$i][2];
-					if ($csv[$i][4] != null)$homepage .="\nPrevisto alle ".$csv[$i][4];
-					if ($csv[$i][5] != null)$homepage .="\n".$csv[$i][5]." alle ".$csv[$i][6];
-					$homepage .="\n____________\n";
-
-
-				}
-				$chunks = str_split($homepage, self::MAX_LENGTH);
-				foreach($chunks as $chunk) {
-					$content = array('chat_id' => $chat_id, 'text' => $chunk,'disable_web_page_preview'=>true);
-					$telegram->sendMessage($content);
-						}
-						$this->create_keyboard_temp($telegram,$chat_id);
-						exit;
-		}
-		elseif($text == "Partenze")
 		{
 
 
@@ -128,11 +85,58 @@ function start($telegram,$update)
 
 
 					$homepage .="\n";
-					$homepage .="Volo N°: ".$csv[$i][0]."\n";
-					$homepage .="Da: ".$csv[$i][1];
+					$homepage .="Volo N°: ".$csv[$i][0];
+					$homepage .=" da ".$csv[$i][1];
 					if ($csv[$i][2] != null) $homepage .=" Aereoporto: ".$csv[$i][2];
 					if ($csv[$i][4] != null)$homepage .="\nPrevisto alle ".$csv[$i][4];
-					if ($csv[$i][5] != null)$homepage .="\n".$csv[$i][5]." alle ".$csv[$i][6];
+					if ($csv[$i][5] != null)$homepage .=" e ".strtolower($csv[$i][5])." alle ".$csv[$i][6];
+					$homepage .="\n____________\n";
+
+
+				}
+				$chunks = str_split($homepage, self::MAX_LENGTH);
+				foreach($chunks as $chunk) {
+					$content = array('chat_id' => $chat_id, 'text' => $chunk,'disable_web_page_preview'=>true);
+					$telegram->sendMessage($content);
+						}
+						$this->create_keyboard_temp($telegram,$chat_id);
+						exit;
+		}
+		elseif($text == "Partenze")
+		{
+
+
+				$urlgd  ="https://spreadsheets.google.com/tq?tqx=out:csv&tq=SELECT%20%2A%20&key=".GDRIVEKEY."&gid=".GDRIVEGID2;
+				sleep (1);
+				$inizio=1;
+				$homepage ="";
+				//$comune="Lecce";
+
+				//echo $urlgd;
+				$csv = array_map('str_getcsv',file($urlgd));
+				//var_dump($csv[1][0]);
+				$count = 0;
+				foreach($csv as $data=>$csv1){
+					$count = $count+1;
+				}
+
+					function decode_entities($text) {
+
+													$text=htmlentities($text, ENT_COMPAT,'ISO-8859-1', true);
+												$text= preg_replace('/&#(\d+);/me',"chr(\\1)",$text); #decimal notation
+													$text= preg_replace('/&#x([a-f0-9]+);/mei',"chr(0x\\1)",$text);  #hex notation
+												$text= html_entity_decode($text,ENT_COMPAT,"UTF-8"); #NOTE: UTF-8 does not work!
+				return $text;
+					}
+				for ($i=$inizio;$i<$count;$i++){
+
+
+					$homepage .="\n";
+					$homepage .="Volo N° ".$csv[$i][0];
+					$homepage .=" per ".$csv[$i][1];
+					if ($csv[$i][2] != null) $homepage .=" Aereoporto di ".$csv[$i][2];
+					if ($csv[$i][4] != null)$homepage .="\nPrevisto alle ".$csv[$i][4];
+					if ($csv[$i][5] != null)$homepage .=" e ".strtolower($csv[$i][5])." alle ".$csv[$i][6];
 					$homepage .="\n____________\n";
 
 
@@ -152,103 +156,12 @@ function start($telegram,$update)
 
 	function create_keyboard_temp($telegram, $chat_id)
 	 {
-			 $option = array(["Partenze","Arrivi"],["Informazioni"]);
+			 $option = array(["🛫 Partenze","🛬 Arrivi"],["❕ Informazioni"]);
 			 $keyb = $telegram->buildKeyBoard($option, $onetime=false);
-			 $content = array('chat_id' => $chat_id, 'reply_markup' => $keyb, 'text' => "[Digita o fai una Scelta]");
+			 $content = array('chat_id' => $chat_id, 'reply_markup' => $keyb, 'text' => "[Fai una Scelta]");
 			 $telegram->sendMessage($content);
 	 }
 
-
-/*
-function location_manager($telegram,$user_id,$chat_id,$location)
-	{
-
-			$lon=$location["longitude"];
-			$lat=$location["latitude"];
-			$response=$telegram->getData();
-			$response=str_replace(" ","%20",$response);
-
-				$reply="http://nominatim.openstreetmap.org/reverse?email=piersoft2@gmail.com&format=json&lat=".$lat."&lon=".$lon."&zoom=18&addressdetails=1";
-				$json_string = file_get_contents($reply);
-				$parsed_json = json_decode($json_string);
-				//var_dump($parsed_json);
-				$comune="";
-				$temp_c1 =$parsed_json->{'display_name'};
-
-				if ($parsed_json->{'address'}->{'town'}) {
-					$temp_c1 .="\nCittà: ".$parsed_json->{'address'}->{'town'};
-					$comune .=$parsed_json->{'address'}->{'town'};
-				}else 	$comune .=$parsed_json->{'address'}->{'city'};
-
-				if ($parsed_json->{'address'}->{'village'}) $comune .=$parsed_json->{'address'}->{'village'};
-				$location="Comune di: ".$comune." tramite le coordinate che hai inviato: ".$lat.",".$lon;
-				$content = array('chat_id' => $chat_id, 'text' => $location,'disable_web_page_preview'=>true);
-				$telegram->sendMessage($content);
-
-			  $alert="";
-				//echo $comune;
-				$urlgd  ="https://spreadsheets.google.com/tq?tqx=out:csv&tq=SELECT%20%2A%20WHERE%20upper(A)%20LIKE%20%27%25";
-				$urlgd .=strtoupper($comune);
-				$urlgd .="%25%27&key=".GDRIVEKEY."&gid=".GDRIVEGID3;
-
-				sleep (1);
-
-					$inizio=1;
-					$homepage ="";
-				  //$comune="Lecce";
-
-				//echo $urlgd;
-					$csv = array_map('str_getcsv',file($urlgd));
-				//var_dump($csv[1][0]);
-				  $count = 0;
-					foreach($csv as $data=>$csv1){
-						$count = $count+1;
-					}
-					if ($count ==0 || $count ==1){
-						$location="Nessun risultato trovato";
-						$content = array('chat_id' => $chat_id, 'text' => $location,'disable_web_page_preview'=>true);
-						$telegram->sendMessage($content);
-					}
-					function decode_entities($text) {
-
-													$text=htmlentities($text, ENT_COMPAT,'ISO-8859-1', true);
-												$text= preg_replace('/&#(\d+);/me',"chr(\\1)",$text); #decimal notation
-													$text= preg_replace('/&#x([a-f0-9]+);/mei',"chr(0x\\1)",$text);  #hex notation
-												$text= html_entity_decode($text,ENT_COMPAT,"UTF-8"); #NOTE: UTF-8 does not work!
-	return $text;
-					}
-				  //echo $count;
-				//  $count=3;
-					for ($i=$inizio;$i<$count;$i++){
-
-						$homepage .="\n";
-						$homepage .="Comune: ".$csv[$i][0]."\n";
-						$homepage .="Indirizzo: ".$csv[$i][1]."\n";
-						$homepage .="CAP: ".$csv[$i][2]."\n";
-						if($csv[$i][3] !=NULL)	$homepage .="Segretario/Referente: ".$csv[$i][3]."\n";
-						if($csv[$i][4] !=NULL) $homepage .="Tel: ".$csv[$i][4]."\n";
-						if($csv[$i][5] !=NULL)$homepage .="Email: ".$csv[$i][5]."\n";
-						if($csv[$i][6] !=NULL){
-							$homepage .= "http://www.openstreetmap.org/?mlat=".$csv[$i][6]."&mlon=".$csv[$i][7]."#map=19/".$csv[$i][6]."/".$csv[$i][7]."/".$_POST['qrname'];
-						}
-					  $homepage .="\n____________\n";
-
-					}
-
-		//}
-
-		//	echo $alert;
-
-			$chunks = str_split($homepage, self::MAX_LENGTH);
-			foreach($chunks as $chunk) {
-				$content = array('chat_id' => $chat_id, 'text' => $chunk,'disable_web_page_preview'=>true);
-				$telegram->sendMessage($content);
-
-			}
-			$this->create_keyboard_temp($telegram,$chat_id);
-
-	}
-*/
 
 }
 
